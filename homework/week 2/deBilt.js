fce6ef53e5b44ffdebb1c25d9f86269d43014dea
MarcMrman <36168546+MarcMrman@users.noreply.github.com>
2018-04-18 15:19:45 +0200

    update

--- homework/week 2/deBilt.js
+++ homework/week 2/deBilt.js
@@ -6,81 +6,96 @@
 
 // retrieve temperature data from html
 var data = document.getElementById("rawdata").innerHTML;
-//console.log(data)
 
 // create arrays after newlines remove the first line
 var lines = data.split('\n').slice(1);
-//console.log(lines)
 
 var dict = [];
 
 // remove whitespaces from strings
 for (var i = 0; i < lines.length; i ++){
-	var lines1 = lines[i].replace(/\s+/g, "");
-	dict.push(lines1);
-	//console.log(lines1)
+	var line = lines[i].replace(/\s+/g, "");
+	dict.push(line);
 }
-//console.log(dict)
 
 var dates = [];
-var temp = [];
-var dict1 = [];
+var temps = [];
 
-// split arrays at comma's and push to seperate arrays
-for(var i = 0; i < dict.length; i ++){
+// split arrays at comma's and push temperatures to array
+for(var i = 0; i < dict.length - 1; i ++){
 	var dayData = dict[i].split(',');
-
-	dates.push(dayData[0]);
-	temp.push(dayData[1]);
-
-	 dict1.push({
-	 	key: dayData[0],
-	 	value: dayData[1]
-	 	})
+	
+	var temp = parseInt(dayData[1])
+	temps.push(temp);
 }
 
-console.log(dict1)
-// console.log(dates)
-// console.log(typeof(dates))
-console.log(temp)
-console.log(typeof(temp))
+// create dates for temperature points SUBSTRINGS van maken om op de goede manier te doen
+for(var i = 17167 ; i < 17532; i ++){
+		var date = new Date(i * 86400000);
+		dates.push(date)
+}
+//console.log(dates)
+//console.log(temps)
 
-var str = string(temp);
-console.log(str)
-//var max = Math.max.apply(Math, temp);
-//console.log(max)
+// find min and max temperature
+var minTemp = Math.min.apply(Math, temps);
+var maxTemp = Math.max.apply(Math, temps);
+console.log(maxTemp)
+console.log(minTemp)
 
-// domain is x-axis
-//var domain = 
-// range is y-axis
-//var range = 
+// find earliest and latest date
+var minDate = Math.min.apply(Math, dates)
+var maxDate = Math.max.apply(Math, dates)
+console.log(minDate)
+console.log(maxDate)
 
-// function createTransform(domain, range){
-// 	// domain is a two-element array of the data bounds [domain_min, domain_max]
-// 	// range is a two-element array of the screen bounds [range_min, range_max]
-// 	// this gives you two equations to solve:
-// 	// range_min = alpha * domain_min + beta
-// 	// range_max = alpha * domain_max + beta
-//  	// a solution would be:
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
-//     var domain_min = domain[0]
-//     var domain_max = domain[1]
-//     var range_min = range[0]
-//     var range_max = range[1]
+var domainTemp = [minTemp, maxTemp]
+var rangeTemp = [20, 400]
+var domainDate = [minDate, maxDate]
+var rangeDate = [20, 500]
 
-//     // formulas to calculate the alpha and the beta
-//    	var alpha = (range_max - range_min) / (domain_max - domain_min)
-//     var beta = range_max - alpha * domain_max
+createTransform(domainTemp, rangeTemp)
+createTransform(domainDate, rangeDate)
 
-//     // returns the function for the linear transformation (y= a * x + b)
-//     return function(x){
-//       return alpha * x + beta;
-//     }
-// }
+function createTransform(domain, range){
+	// domain is a two-element array of the data bounds [domain_min, domain_max]
+	// range is a two-element array of the screen bounds [range_min, range_max]
+	// this gives you two equations to solve:
+	// range_min = alpha * domain_min + beta
+	// range_max = alpha * domain_max + beta
+ 	// a solution would be:
 
-// var canvas = document.getElementById("myCanvas");
-// //canvas.width = 300;
-// //canvas.length = 300;
-// var ctx = canvas.getContext("2d");
-// ctx.fillStyle = "rgb(0,256,256)";
-// ctx.fillRect(50, 50, 500, 500);
+    var domain_min = domain[0]
+    var domain_max = domain[1]
+    var range_min = range[0]
+    var range_max = range[1]
+
+    // formulas to calculate the alpha and the beta
+   	var alpha = (range_max - range_min) / (domain_max - domain_min)
+    var beta = range_max - alpha * domain_max
+
+    console.log(alpha)
+    console.log(beta)
+    // returns the function for the linear transformation (y= a * x + b)
+    return function(x){
+      return alpha * x + beta;
+    }
+}
+
+// Graph axis
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.lineTo(20, 400);
+ctx.lineTo(500, 400);
+ctx.stroke();
+
+// text in graph
+// ctx.font = "24px arial";
+// ctx.strokeText("Average temperature deBilt (2017)", 200, 20)
+// ctx.font = "10px arial"
+// ctx.fillText("Average temperature", 0, 10);
+// ctx.font = "10px arial"
+// ctx.fillText("Date", 450, 420);
