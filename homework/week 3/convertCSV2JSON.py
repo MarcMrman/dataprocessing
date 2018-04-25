9b5b46dad8471211059bea0f577679d63afe731d
MarcMrman <36168546+MarcMrman@users.noreply.github.com>
2018-04-25 15:32:18 +0200

    homework update

--- homework/week 3/convertCSV2JSON.py
+++ homework/week 3/convertCSV2JSON.py
@@ -5,17 +5,16 @@ import json
 csv_file = "./Canadian population 1851-2001.csv"
 json_file = "Canadian population 1851-2001.json"
 
-# load csv file into a list
 with open(csv_file, "r") as infile:
-    reader = csv.DictReader(infile)
-    csv_list = list(reader)
-
-print(csv_list)
+  reader = csv.DictReader(infile)
+  csv_list = list(reader)
 
 # creating and updating dict with data
-data = {}
-data.update(dict(data=csv_list))
+# data = {}
+# data.update(dict(data=csv_list))
 
-# write data into json
+# write data into json TERUG IN DICT ZETTEN, NIET ZO HARDCODEN
 with open (json_file, 'w') as outfile:
-	json.dump(data, outfile)
+   outfile.write('{"data":')
+   json.dump(csv_list, outfile)
+   outfile.write('}')
