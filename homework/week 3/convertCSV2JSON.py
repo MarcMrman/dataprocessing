9366ffb546be86a0ec95be4a18e8678dc6e1fc27
MarcMrman <36168546+MarcMrman@users.noreply.github.com>
2018-04-26 13:54:14 +0200

    homework done

--- homework/week 3/convertCSV2JSON.py
+++ homework/week 3/convertCSV2JSON.py
@@ -2,19 +2,17 @@ import csv
 import json
 
 # local file path, MOET DIT ZO VAN INTERNET AFGEHAALD WORDEN?
-csv_file = "./Canadian population 1851-2001.csv"
-json_file = "Canadian population 1851-2001.json"
+csv_file = "./Canadian population 1850-2000.csv"
+json_file = "Canadian population 1850-2000.json"
 
 with open(csv_file, "r") as infile:
   reader = csv.DictReader(infile)
   csv_list = list(reader)
 
 # creating and updating dict with data
-# data = {}
-# data.update(dict(data=csv_list))
+data = {}
+data.update(dict(data=csv_list))
 
 # write data into json TERUG IN DICT ZETTEN, NIET ZO HARDCODEN
 with open (json_file, 'w') as outfile:
-   outfile.write('{"data":')
-   json.dump(csv_list, outfile)
-   outfile.write('}')
+   json.dump(data, outfile)
