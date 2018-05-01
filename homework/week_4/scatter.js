b814880a6a4e90ef5b0d14d151f0e6aecee3d522
MarcMrman <36168546+MarcMrman@users.noreply.github.com>
2018-05-01 16:53:23 +0200

    hij scattered

--- homework/week_4/scatter.js
+++ homework/week_4/scatter.js
@@ -12,9 +12,10 @@ window.onload = function() {
 	// min wage = http://stats.oecd.org/BrandedView.aspx?oecd_bv_id=lfs-data-en&doi=data-00656-en
 
 	// using api's to retrieve data
-	var GDP = "http://stats.oecd.org/SDMX-JSON/data/PDB_LV/AUS+BEL+EST+FRA+DEU+GRC+HUN+IRL+JPN+LVA+LUX+NLD+NZL+POL+PRT+SVN+ESP+GBR+USA+LTU.T_GDPPOP.VPVOB/all?startTime=2004&endTime=2013&dimensionAtObservation=allDimensions"
-	var realMinWage = "http://stats.oecd.org/SDMX-JSON/data/RMW/AUS+BEL+EST+FRA+DEU+GRC+HUN+IRL+JPN+LVA+LUX+NLD+NZL+POL+PRT+SVN+ESP+GBR+USA+LTU.PPP.A/all?startTime=2004&endTime=2013&dimensionAtObservation=allDimensions";
-	//var hoursWorkedAnnually = "http://stats.oecd.org/SDMX-JSON/data/ANHRS/AUS+BEL+EST+FRA+DEU+GRC+HUN+IRL+JPN+LVA+LUX+NLD+NZL+POL+PRT+SVN+ESP+GBR+USA+LTU.TE.A/all?startTime=2004&endTime=2013&dimensionAtObservation=allDimensions"
+	// LIJSTEN HEBBEN NIET ZELFDE LANDEN VOLGORDE
+	var GDP = "http://stats.oecd.org/SDMX-JSON/data/PDB_LV/AUS+BEL+EST+FRA+GRC+HUN+IRL+JPN+LVA+LUX+NLD+NZL+POL+PRT+SVN+ESP+GBR+LTU.T_GDPPOP.VPVOB/all?startTime=2004&endTime=2013&dimensionAtObservation=allDimensions"
+	var realMinWage = "http://stats.oecd.org/SDMX-JSON/data/RMW/AUS+BEL+EST+FRA+GRC+HUN+IRL+JPN+LVA+LUX+NLD+NZL+POL+PRT+SVN+ESP+GBR+LTU.PPP.A/all?startTime=2004&endTime=2013&dimensionAtObservation=allDimensions";
+	//var hoursWorkedAnnually = "http://stats.oecd.org/SDMX-JSON/data/ANHRS/AUS+BEL+EST+FRA+GRC+HUN+IRL+JPN+LVA+LUX+NLD+NZL+POL+PRT+SVN+ESP+GBR+LTU.TE.A/all?startTime=2004&endTime=2013&dimensionAtObservation=allDimensions"
 	//var kleuren = Noord/west/zuid/oost europa 
 
 	// queuing steps to do before drawing plot
@@ -32,20 +33,91 @@ window.onload = function() {
 	  // writing JSON
 	  else 
 	  	var gdpJSON = JSON.parse(response[0].responseText);
-	  	console.log(gdpJSON)
+	  	//console.log(gdpJSON)
 	  	var wageJSON = JSON.parse(response[1].responseText);
-	  	console.log(wageJSON)
-	  	//var hoursJSON = JSON.parse(response[2].responseText);
-	  	//console.log(hoursJSON)
+	  	//console.log(wageJSON)
+	  	// var hoursJSON = JSON.parse(response[2].responseText);
+	  	// console.log(hoursJSON)
 
-	  	console.log(gdpJSON.dataSets[0].observations["0:0:0:0"][0])
+	  	// console.log(gdpJSON.structure.dimensions.observation[0].values[0].name)
+	  	
+	  	// var sorted = gdpJSON.sort(function(a, b){
+	  	// 	for (var i = 0; i < 19; i ++)
+	  	// 		if (a.structure.dimensions.observation[0].values[i].name < b.structure.dimensions.observation[0].values[i].name) return -1;
+	  	// 		if (a.structure.dimensions.observation[0].values[i].name > b.structure.dimensions.observation[0].values[i].name) return 1;
+	  	// 		return 0;
+  		// 	};
+	  	// console.log(sorted)	
+	  	// });
 
-	  	for (var i = 0; i < response.dataSets.observations.length`1``	; i ++){
-		  	console.log(gdpJSON.dataSets[0].observations[i + ":0:0:0"][0])
-		  	console.log(wageJSON.dataS																																11	ets[0].observations[i + ":0:0:0"][0])
-		  	//console.log(hoursJSON.dataSets[0].observations[i + ":0:0:0"][0])
-	  	}
+	  	// putting info in seperate arrays
+	  	var gdp_PY = [];
+	  	var wage_PY = [];
+	  	for (var j = 0; j < 10; j ++){
+	  		var yearlyGDP = [];
+	  		var yearlyWage = [];
 
+	  		for (var i = 0; i < 18; i ++){
+	  			yearlyGDP.push(gdpJSON.dataSets[0].observations[i + ":0:0:" + j][0]);
+	  			yearlyWage.push(wageJSON.dataSets[0].observations[i + ":" + j +":0:0"][0]);
+	  		};
+	  		gdp_PY.push(yearlyGDP)
+	  		wage_PY.push(yearlyWage)
+	  	};
+	  	console.log(gdp_PY)
+	  	console.log(wage_PY)
+
+	  	// lijsten samenvoegen om zo coordinaten te krijgen
+	  	console.log(gdp_PY[0][0])
+	  	console.log(wage_PY[0][0])
+
+	  	var dataset = [];
+
+	  	for (var i = 0; i < gdp_PY[0].length; i ++){
+	  		var x_yGdpWage = [];
+	  		x_yGdpWage.push(gdp_PY[0][i])
+	  		x_yGdpWage.push(wage_PY[0][i])
+	  		dataset.push(x_yGdpWage)
+	  	};
+	  	console.log(dataset)
+
+	  	//Create SVG element
+	  	var w = 1000
+	  	var h = 500
+	  	// var leftMargin = 
+	  	// var rightMarging =
+	  	// var topMargin =
+	  	// var bottomMargin = 
+
+		var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        // creating circles
+        svg.selectAll("circle")
+		   .data(gdp_PY[0])
+		   .enter()
+		   .append("circle")
+		   .attr("class", "circle")
+		   .attr("cx", function(d, i){
+		   		return dataset[i][0] / w;
+		   })
+   			.attr("cy", function(d, i){
+		   		return dataset[i][1] / h;
+		   	})
+   			.attr("r", 4);
 	};
+
+	// function getData() --> deze in window.onload uit laten voeren
+	// alle functies er buiten definieren
+
+	// function makeScater
+
+	// function addLegend etc.
 };
 
+// function(d, i) {
+// 			    console.log(dataset[i][1])
+// 			    return (Math.sqrt(h - dataset[i][1])) / 10000;
+// 			}
